74b3779642b7b91349c0e283ab0762c5b16894b0
Karastan Vladislav <90850632+Kar1stan@users.noreply.github.com>
2023-02-24 21:06:18 +0300

    Add files via upload

---
+++ PythonTestTask/pages/contact_us_page.py
@@ -0,0 +1,31 @@
+from playwright.sync_api import Page
+
+
+class TestContactUsPage:
+    def __init__(self, page: Page):
+        self.page = page
+        self.contact_us_button = page.locator("//a[text()=' Contact us']")
+        self.header_get_in_touch = page.locator("//h2[text()='Get In Touch']")
+        self.name_input = page.locator("input[data-qa='name']")
+        self.email_input = page.locator("input[data-qa='email']")
+        self.subject_input = page.locator("input[data-qa='subject']")
+        self.message_input = page.locator("#message")
+        self.submit_button = page.locator("input[data-qa='submit-button']")
+
+    def click_contact_us_button(self):
+        self.contact_us_button.click()
+
+    def fill_name(self, name):
+        self.name_input.fill(name)
+
+    def fill_email(self, email):
+        self.email_input.fill(email)
+
+    def fill_subject(self, subject):
+        self.subject_input.fill(subject)
+
+    def fill_message(self, message):
+        self.message_input.fill(message)
+
+    def click_submit_button(self):
+        self.submit_button.click()
---
+++ PythonTestTask/pages/login_user_page.py
@@ -0,0 +1,24 @@
+from playwright.sync_api import Page
+
+
+class TestLoginUserPage:
+    def __init__(self, page: Page):
+        self.page = page
+        self.header_login_to_your_account = page.locator("//h2[text()='Login to your account']")
+        self.login_email_input = page.locator("input[data-qa='login-email']")
+        self.login_password_input = page.locator("input[data-qa='login-password']")
+        self.login_button = page.locator("input[data-qa='login-button']")
+        self.login_error = page.locator("//p[text()='Your email or password is incorrect!']")
+        self.logout_button = page.locator("//a[text()=' Logout']")
+
+    def fill_login_email(self, email):
+        self.login_email_input.fill(email)
+
+    def fill_login_password(self, password):
+        self.login_password_input.fill(password)
+
+    def click_login_button(self):
+        self.login_button.click()
+
+    def click_logout_button(self):
+        self.logout_button.click()
